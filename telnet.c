26bd67ded454e5a869809f5eb81ba4801be89f30
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-04-24 12:11:35 -0700

    Added support for IPv4, IPv6, and hostname resolution.

--- telnet.c
+++ telnet.c
@@ -8,6 +8,7 @@
 #include <signal.h>
 #include <pthread.h>
 #include <termios.h>
+#include <netdb.h>
 
 // typedefs
 typedef unsigned char byte;
@@ -22,19 +23,12 @@ byte DONT = 254;
 byte IAC = 255;
 
 // GLOBALS
-pthread_t data_thread, input_thread;
+pthread_t input_thread;
 int fd;
 char read_buffer[BUFFSIZE] = {0};
 char write_buffer[BUFFSIZE] = {0};
 struct termios oldt, newt;
 
-int fail(const char* msg, int file_descriptor, int exitcode) {
-    printf("%s\n", msg);
-    if (file_descriptor != 0) {
-        close(file_descriptor);
-    }
-    exit(exitcode);
-}
 
 byte deny(byte opt_code) {
     if (opt_code == WONT) return DONT;
@@ -50,76 +44,21 @@ enum states {
     s2 = 2,
 } state;
 
+void shutdown_program() {
+    printf("\nCaught signal, shutting down...\n");
+    pthread_cancel(input_thread);
+    pthread_join(input_thread, NULL);
+    // Restore old terminal settings.
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    close(fd);
+    exit(1);
+}
+
 void sig_handler(int signal)
 {
     if (signal == SIGINT || signal == SIGTERM) {
-        printf("\nCaught signal, closing socket and ending threads...\n");
-        pthread_cancel(data_thread);
-        pthread_cancel(input_thread);
-        pthread_join(input_thread, NULL);
-        pthread_join(data_thread, NULL);
-        // Restore old terminal settings.
-        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-        close(fd);
-        exit(1);
+        shutdown_program();
     }
-}
-
-void* receive_handler(void* arg) {
-
-    // Enable thread cancel.
-    pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
-    pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
-
-    state = initial;
-    int num_bytes;
-    byte opt_code;
-    int write_buffer_index = 0;
-
-    while (1) {
-        num_bytes = recv(fd, read_buffer, BUFFSIZE, 0);
-        if (num_bytes == -1) {
-            fail("Error reading from socket.", fd, 1);
-        }
-        for (int i = 0; i < num_bytes; i++) {
-            byte current = (byte) read_buffer[i];
-            if (state == initial) {
-                if (current != IAC) {
-                    printf("%c", current);
-                    fflush(stdout);
-                    continue;
-                }
-                state = s1;
-                continue;
-            }
-            else if (state == s1) {
-                if (current != WILL && current != DO && current != DONT && current != WONT) {
-                    fail("Error in state 1.", fd, 1);
-                }
-                opt_code = current;
-                state = s2;
-                continue;
-            }
-            else if (state == s2) {
-                // Write IAC opt_code current to write buffer.
-                // By default, I am denying all DOs and denying all WILLs.
-                write_buffer[write_buffer_index++] = IAC;
-                write_buffer[write_buffer_index++] = deny(opt_code);
-                write_buffer[write_buffer_index++] = current;
-                state = initial;
-                continue;
-            }
-        }
-        if (strlen(write_buffer) > 0) {
-            if (send(fd, write_buffer, strlen(write_buffer), 0) == -1) {
-                fail("Failed to send", fd, 1);
-            }
-            memset(write_buffer, '\0', BUFFSIZE);
-            write_buffer_index = 0;
-        }
-        memset(read_buffer, '\0', BUFFSIZE);
-    }
-    return NULL;
 }
 
 void *input_handler(void* arg) {
@@ -158,40 +97,108 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
-    printf("IP: %s\n", ip);
-    printf("PORT: %d\n", port);
+    struct addrinfo hints;
+    struct addrinfo *res;
 
-    struct sockaddr_in server_address;
+    // Support both IPv4 and IPv6 addresses over TCP.
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
 
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(port);
-    if(inet_pton(AF_INET, ip, &server_address.sin_addr) <= 0) {
-        printf("IP address conversion failed.\n");
+    // Perform DNS hostname resolution.
+    if (getaddrinfo(ip, "telnet", &hints, &res) != 0) {
+        perror("DNS resolution failed.");
         exit(1);
     }
 
-    fd = socket(AF_INET, SOCK_STREAM, 0);
+    fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
     if (fd < 0) {
-        printf("Socket creation failed.\n");
+        perror("Socket creation failed.");
         exit(1);
     }
 
-    if(connect(fd, (struct sockaddr*) &server_address, sizeof(server_address)) < 0) {
-        printf("Failed to connect to address.\n");
+    if (connect(fd, res->ai_addr, res->ai_addrlen) == -1) {
+        perror("Failed to connect.");
         exit(1);
     }
+
+    // We can free this memory now.
+    freeaddrinfo(res);
 
     memset(write_buffer, '\0', sizeof(char)*BUFFSIZE);
     memset(read_buffer, '\0', sizeof(char)*BUFFSIZE);
 
     // Make threads.
     pthread_create(&input_thread, NULL, input_handler, NULL);
-    pthread_create(&data_thread, NULL, receive_handler, NULL);
 
+    // ---- Main data processing loop ----
+    state = initial;
+    int num_bytes;
+    byte opt_code;
+    int write_buffer_index = 0;
+
+    while (1) {
+        num_bytes = recv(fd, read_buffer, BUFFSIZE, 0);
+        if (num_bytes == -1) {
+            perror("Error reading from socket.");
+            close(fd);
+            exit(1);
+        }
+        if (num_bytes == 0) {
+            break;
+        }
+        for (int i = 0; i < num_bytes; i++) {
+            byte current = (byte) read_buffer[i];
+            if (state == initial) {
+                if (current != IAC) {
+                    printf("%c", current);
+                    fflush(stdout);
+                    continue;
+                }
+                state = s1;
+                continue;
+            }
+            else if (state == s1) {
+                if (current != WILL && current != DO && current != DONT && current != WONT) {
+                    perror("Error in state 1.");
+                    close(fd);
+                    exit(1);
+                }
+                opt_code = current;
+                state = s2;
+                continue;
+            }
+            else if (state == s2) {
+                // Write IAC opt_code current to write buffer.
+                // By default, I am denying all DOs and denying all WILLs.
+                if (opt_code == WILL && current == 1) {
+                    state = initial;
+                    continue;
+                }
+                write_buffer[write_buffer_index++] = IAC;
+                write_buffer[write_buffer_index++] = deny(opt_code);
+                write_buffer[write_buffer_index++] = current;
+                state = initial;
+                continue;
+            }
+        }
+        if (strlen(write_buffer) > 0) {
+            if (send(fd, write_buffer, strlen(write_buffer), 0) == -1) {
+                perror("Failed to send.");
+                close(fd);
+                exit(1);
+            }
+            memset(write_buffer, '\0', BUFFSIZE);
+            write_buffer_index = 0;
+        }
+        memset(read_buffer, '\0', BUFFSIZE);
+    }
+
+    // Server closed connection, shutdown threads, restore terminal,
+    // and close socket descriptor.
+    pthread_cancel(input_thread);
     pthread_join(input_thread, NULL);
-    pthread_join(data_thread, NULL);
-
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
     close(fd);
     return 0;
 };
-
